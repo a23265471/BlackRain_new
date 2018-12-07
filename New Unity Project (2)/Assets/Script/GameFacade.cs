0efd6b0c8204e211eaa7748137d051dd4c702ace
2F-04\Intern <a23265471@gmail.com>
2018-12-07 17:30:27 +0800

    12/7 嶄新的世界

---
+++ New Unity Project (2)/Assets/Script/GameFacade.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFacade : MonoBehaviour {
+
+    private static GameFacade instance;
+  
+    public GameFacade GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = GameObject.FindObjectOfType<GameFacade>();
+            if (instance == null)
+            {
+                throw new System.Exception("GameFacade不存在於場景中，請在場景中添加");
+            }
+            instance.Initialize();
+        }
+        return instance;
+    }
+
+    #region Controller
+
+    #endregion
+
+
+    #region Models
+    public PlayerStageData playerStageData;
+
+    #endregion
+
+
+
+    private void Initialize()
+    {
+
+        playerStageData = new PlayerStageData();
+
+    }
+
+    private void Awake()
+    {
+        GetInstance();
+
+    }
+}
