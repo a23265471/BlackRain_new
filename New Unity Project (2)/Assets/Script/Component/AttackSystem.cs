cca21fc6c82cfd04d6cc7978b731227256584449
PC-20180311YEZK\user <a23265471@gmail.com>
2019-04-10 01:32:25 +0800

    0410 Attack List

--- New Unity Project (2)/Assets/Script/Component/AttackSystem.cs
+++ New Unity Project (2)/Assets/Script/Component/AttackSystem.cs
@@ -3,19 +3,116 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
-public class AttackSystem : MonoBehaviour {
-
+public class AttackSystem : MonoBehaviour
+{
+    public SkillList skillList;
     private Animator animator;
+    private Dictionary<int, SkillList.AttackParameter> AttackCollection;
 
-	// Use this for initialization
-	void Start ()
+    private bool CanTriggerNextAttack;
+    private bool isTriggerAttack;
+
+
+    private void Awake()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        CreateAttackCollection();
+        animator = GetComponent<Animator>();
+    }
+
+    #region 建立攻擊列表
+    public void CreateAttackCollection()
     {
-		
-	}
+        SkillList.AttackParameter AttackParameter;
+
+        AttackCollection = new Dictionary<int, SkillList.AttackParameter>();
+
+        for (int i=0;i< skillList.normalAttack.Length; i++)
+        {
+            if (AttackCollection.TryGetValue(skillList.normalAttack[i].Id, out AttackParameter))
+            {
+                throw new System.Exception("Normal Attack Id已重複");
+
+            }
+            else
+            {
+                AttackCollection[skillList.normalAttack[i].Id] = skillList.normalAttack[i];
+            }
+         //   Debug.Log(skillList.normalAttack[i].Id);
+
+        }
+
+        for (int i = 0; i < skillList.specialAttack.Length; i++)
+        {
+            if (AttackCollection.TryGetValue(skillList.specialAttack[i].Id, out AttackParameter) )
+            {
+                throw new System.Exception("Special Attack Id已重複 specialAttack" +i+"Id = " + skillList.specialAttack[i].Id);
+
+            }
+            else
+            {
+                AttackCollection[skillList.specialAttack[i].Id] = skillList.specialAttack[i];
+
+            }
+        }
+
+        for (int j = 0; j < skillList.deputyAttackCollections.Length; j++) 
+        {
+            for (int i = 0; i < skillList.deputyAttackCollections[j].DeputyAttack.Length; i++)
+            {
+                if (AttackCollection.TryGetValue(skillList.deputyAttackCollections[j].DeputyAttack[i].Id, out AttackParameter))
+                {
+                    throw new System.Exception("deputy Attack Collections "+ j + " DeputyAttack "+ i +"Id已重複 Id="+ skillList.deputyAttackCollections[j].DeputyAttack[i].Id);
+
+                }
+                else
+                {
+                    AttackCollection[skillList.deputyAttackCollections[j].DeputyAttack[i].Id] = skillList.deputyAttackCollections[j].DeputyAttack[i];
+
+                }
+            }
+        }
+
+       
+    }
+    #endregion
+
+    public void JudgeInputKey(KeyCode input)
+    {
+
+
+    }
+
+
+
+    public void NormalAttack()
+    {
+        if (CanTriggerNextAttack)
+        {
+            // Debug.Log("gggg");
+            animator.SetTrigger("NormalAttack");
+            CanTriggerNextAttack = false;
+            isTriggerAttack = true;
+        }
+
+    }
+
+
+
+
+
+
+
+
+    #region 動畫事件
+    public void GetAttackInfo(int Id)
+    {
+
+    }
+
+   
+
+
+    #endregion 
+
+
 }
---
+++ New Unity Project (2)/Assets/Script/Models/SkillList.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SkillList : ScriptableObject
+{
+    [System.Serializable]
+    public struct AttackParameter
+    {
+        public string Name;
+        public int Id;
+        public int AttackOder;
+        public ChangeToDeputyAttack[] deputyAttack;
+        public float MoveSpeed;
+        public float MoveDistance;
+        public int MoveDirection_X;
+        public int MoveDirection_Y;
+        public int MoveDirection_Z;
+        public bool UseGravity;
+
+        public AudioClip AudioClip_Attack;
+        // public GameObject Particle_Attack;    
+    }
+
+    [System.Serializable]
+    public struct DeputyAttackCollection
+    {
+        public AttackParameter[] DeputyAttack;
+    }
+
+    [System.Serializable]
+    public struct ChangeToDeputyAttack
+    {
+        public string Id;
+        public KeyCode keyCode;
+    }
+
+    public AttackParameter[] normalAttack;  
+    public DeputyAttackCollection[] deputyAttackCollections;
+    public AttackParameter[] specialAttack;
+}
